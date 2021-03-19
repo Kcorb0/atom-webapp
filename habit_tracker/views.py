8996b2a5e9a4b6133fa97bc08a95eb1f1038174d
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-03-19 00:03:05 +0000

    Resized grid elements for habits display

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -29,7 +29,7 @@ def home(request):
     context = {
         'title': 'Home',
         'habits': habits_example,
-        'dates_list': datelist(7)
+        'dates_list': datelist(10)
     }
     return render(request, 'habit_tracker/home.html', context)
 
